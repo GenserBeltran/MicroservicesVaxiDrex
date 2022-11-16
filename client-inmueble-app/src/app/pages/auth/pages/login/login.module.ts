927c9f2012e2e8c6dbf0cbd61c4634484d32c66a
Daniel Beltran <genserdanie-123@hotmail.com>
2022-11-16 20:15:34 +0100

    Class_prac Login_Registration_RutasActualizadas

---
+++ client-inmueble-app/src/app/pages/auth/pages/login/login.module.ts
@@ -0,0 +1,30 @@
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoginRoutingModule } from './login-routing.module';
+import { LoginComponent } from './login.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    LoginRoutingModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    FormsModule,
+    MatIconModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatInputModule
+  ]
+})
+export class LoginModule { }
---
+++ client-inmueble-app/src/app/pages/auth/pages/registration/registration.component.ts
@@ -0,0 +1,20 @@
+import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.scss']
+})
+export class RegistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  registrarUsuario(form: NgForm) {
+
+  }
+
+}
